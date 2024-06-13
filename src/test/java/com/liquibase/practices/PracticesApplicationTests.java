ca55632c4c8a40820c64451a035334faaf7e0876
sginko <s.ginkota@gmail.com>
2024-06-13 17:39:02 +0200

    Initial commit

---
+++ src/test/java/com/liquibase/practices/PracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibase.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
